fe2caeeaa149d2fdea6b5c9ef9729f030efbb12c
maiklshetinin <sh_chetinin@mail.ru>
2022-08-19 18:09:22 +0300

    feat: add axios

--- src/pages/TextBook/TextBook.tsx
+++ src/pages/TextBook/TextBook.tsx
@@ -1,7 +1,11 @@
-import axios from 'axios';
 import React from 'react';
+import axios from 'axios';
 
 export function TextBook() {
+  axios.get('https://rslang-mdg.herokuapp.com/words')
+    .then((response) => {
+      console.log(response.data);
+    });
 
   return (
     <div>
