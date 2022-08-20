35da2896543d4ca8c1cae247216afe2063cb9428
Greg <martinstudyweb@gmail.com>
2022-08-20 19:07:39 +0300

    feat: add store on index.tsx file

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
 
+import { rootReducer } from './store/reducer/rootReducer';
 import { App } from './App';
 
 import './index.css';
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk,
+  ),
+));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
