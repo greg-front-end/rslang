71f837ea8067f46fcd9d38aafa0ba1eb2a3c4a9b
Greg <martinstudyweb@gmail.com>
2022-08-24 20:25:09 +0300

    feat: add header log out structure and img logo

--- src/components/Header/HeaderLogOut/HeaderLogOut.tsx
+++ src/components/Header/HeaderLogOut/HeaderLogOut.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import LogoImg from '../../../assets/img/logo/dark/logo-desktop-dark.png';
+
+import style from './style.module.css';
+
 export const HeaderLogOut = () => (
-  <>
-    <h1>Header user log out</h1>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="textbook">TextBook</Link>
-      <Link to="games">Games</Link>
-      <Link to="about">About</Link>
-    </nav>
-  </>
+  <header className={style.header}>
+    <div className="container">
+      <div className={style.wrapper}>
+        <div className={style.logo}>
+          <img src={LogoImg} alt="Logo RSLang" />
+        </div>
+        <nav className={style.nav}>
+          <Link className={`${style.link} ${style.active}`} to="/">Home</Link>
+          <Link className={style.link} to="textbook">TextBook</Link>
+          <Link className={style.link} to="games">Play and learn</Link>
+          <Link className={style.link} to="about">About</Link>
+        </nav>
+        <div className={style.auth_wrapper}>
+          <button type="button" className="btn">Log in</button>
+          <button type="button" className={`${style.btn_register} btn`}>Register</button>
+        </div>
+      </div>
+    </div>
+  </header>
 );
