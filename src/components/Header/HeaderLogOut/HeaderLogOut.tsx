13e27675a5a4579926e8a8c3f715fc386fe9e345
Greg <martinstudyweb@gmail.com>
2022-08-25 13:27:57 +0300

    refactor: replace button to link for login and register

--- src/components/Header/HeaderLogOut/HeaderLogOut.tsx
+++ src/components/Header/HeaderLogOut/HeaderLogOut.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import LogoImg from '../../../assets/img/logo/dark/logo-desktop-dark.png';
 
@@ -13,14 +13,14 @@ export const HeaderLogOut = () => (
           <img src={LogoImg} alt="Logo RSLang" />
         </div>
         <nav className={style.nav}>
-          <Link className={`${style.link} ${style.active}`} to="/">Home</Link>
-          <Link className={style.link} to="textbook">TextBook</Link>
-          <Link className={style.link} to="games">Play and learn</Link>
-          <Link className={style.link} to="about">About</Link>
+          <NavLink className={`${style.link} ${style.active}`} to="/">Home</NavLink>
+          <NavLink className={style.link} to="textbook">TextBook</NavLink>
+          <NavLink className={style.link} to="games">Play and learn</NavLink>
+          <NavLink className={style.link} to="about">About</NavLink>
         </nav>
         <div className={style.auth_wrapper}>
-          <button type="button" className="btn">Log in</button>
-          <button type="button" className={`${style.btn_register} btn`}>Register</button>
+          <NavLink to="log-in" className="btn">Log in</NavLink>
+          <NavLink to="register" className={`${style.btn_register} btn`}>Register</NavLink>
         </div>
       </div>
     </div>
