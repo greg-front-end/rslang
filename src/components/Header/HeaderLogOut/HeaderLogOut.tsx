75fccc56753a295d23cf8299ea5f10c7a1ad8b32
Greg <martinstudyweb@gmail.com>
2022-08-25 13:53:14 +0300

    feat: add logic and  style for active link

--- src/components/Header/HeaderLogOut/HeaderLogOut.tsx
+++ src/components/Header/HeaderLogOut/HeaderLogOut.tsx
@@ -5,24 +5,27 @@ import LogoImg from '../../../assets/img/logo/dark/logo-desktop-dark.png';
 
 import style from './style.module.css';
 
-export const HeaderLogOut = () => (
-  <header className={style.header}>
-    <div className="container">
-      <div className={style.wrapper}>
-        <div className={style.logo}>
-          <img src={LogoImg} alt="Logo RSLang" />
-        </div>
-        <nav className={style.nav}>
-          <NavLink className={`${style.link} ${style.active}`} to="/">Home</NavLink>
-          <NavLink className={style.link} to="textbook">TextBook</NavLink>
-          <NavLink className={style.link} to="games">Play and learn</NavLink>
-          <NavLink className={style.link} to="about">About</NavLink>
-        </nav>
-        <div className={style.auth_wrapper}>
-          <NavLink to="log-in" className="btn">Log in</NavLink>
-          <NavLink to="register" className={`${style.btn_register} btn`}>Register</NavLink>
+export const HeaderLogOut = () => {
+  const activeLink = ({ isActive }: {isActive: boolean}) => (isActive ? `${style.link} ${style.active}` : style.link);
+  return (
+    <header className={style.header}>
+      <div className="container">
+        <div className={style.wrapper}>
+          <div className={style.logo}>
+            <img src={LogoImg} alt="Logo RSLang" />
+          </div>
+          <nav className={style.nav}>
+            <NavLink className={activeLink} to="/">Home</NavLink>
+            <NavLink className={activeLink} to="textbook">TextBook</NavLink>
+            <NavLink className={activeLink} to="games">Play and learn</NavLink>
+            <NavLink className={activeLink} to="about">About</NavLink>
+          </nav>
+          <div className={style.auth_wrapper}>
+            <NavLink to="log-in" className={`${style.auth_link} btn`}>Log in</NavLink>
+            <NavLink to="register" className={`${style.btn_register} ${style.auth_link} btn`}>Register</NavLink>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
