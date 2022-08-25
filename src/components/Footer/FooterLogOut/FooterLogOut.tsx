847baed7b0c66ee9fefeb9aa85e23faf69fb1199
Greg <martinstudyweb@gmail.com>
2022-08-25 16:37:01 +0300

    refactor: add rslogo and copyright and some style

--- src/components/Footer/FooterLogOut/FooterLogOut.tsx
+++ src/components/Footer/FooterLogOut/FooterLogOut.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import Logo from '../../../assets/img/logo/dark/logo-desktop-dark.png';
 
-import FooterImg from './footer-img.png';
+import FooterImg from './RocketBoy.png';
+import { ReactComponent as RsLogo } from './rslogo.svg';
 
+import styleLogOut from '../../../style/log_out.module.css';
 import style from './style.module.css';
 
 export const FooterLogOut = () => (
@@ -12,25 +14,33 @@ export const FooterLogOut = () => (
       <div className={style.wrapper}>
         <img src={FooterImg} alt="Rocket boy" />
         <div className={style.team}>
-          <a href="https://github.com/maiklshetinin" target="_blank" rel="noopener noreferrer">maiklshetinin</a>
-          <a href="https://github.com/LilithPrimary" target="_blank" rel="noopener noreferrer">
+          <h4 className={style.title}>Team</h4>
+          <a className={styleLogOut.link} href="https://github.com/maiklshetinin" target="_blank" rel="noopener noreferrer">maiklshetinin</a>
+          <a className={styleLogOut.link} href="https://github.com/LilithPrimary" target="_blank" rel="noopener noreferrer">
             <span>Denna</span>
             LilithPrimary
           </a>
-          <a href="https://github.com/greg-front-end" target="_blank" rel="noopener noreferrer">
+          <a className={styleLogOut.link} href="https://github.com/greg-front-end" target="_blank" rel="noopener noreferrer">
             <span>Greg Martinos</span>
             greg-front-end
           </a>
         </div>
         <div className={style.footer_nav}>
+          <h4 className={style.title}>Menu</h4>
           <nav className={style.nav}>
-            <a className="link_out" href="#hero">Home</a>
-            <a className="link_out" href="#our-advantages">Our advantages</a>
-            <a className="link_out" href="#about-us">About us</a>
+            <a className={styleLogOut.link} href="#hero">Home</a>
+            <a className={styleLogOut.link} href="#our-advantages">Our advantages</a>
+            <a className={styleLogOut.link} href="#about-us">About us</a>
           </nav>
         </div>
-        <img src={Logo} alt="Rocket boy" />
+        <div className={styleLogOut.logo}>
+          <img src={Logo} alt="Rocket boy" />
+        </div>
       </div>
+    </div>
+    <div className={style.copyright}>
+      ©2022 RS LANG. RS School
+      <RsLogo />
     </div>
   </footer>
 );
