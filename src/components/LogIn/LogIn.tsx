74db5d1eb1108f77a737c3c7cdd84326d50c7148
Greg <martinstudyweb@gmail.com>
2022-08-28 14:19:46 +0300

    feat: add styles for forms log in and signin

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -7,6 +7,8 @@ import React, {
 import { useNavigate } from 'react-router-dom';
 
 import { logIn } from '../../api/logIn';
+import { ReactComponent as EmailICon } from '../../assets/svg/auth/email.svg';
+import { ReactComponent as KeyIcon } from '../../assets/svg/auth/key.svg';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { ICreateUser } from '../../types/ICreateUser';
@@ -51,29 +53,45 @@ export const LogIn = () => {
     }
   }, [navigate, userState.token?.token]);
   return (
-    <div className={style.log_in}>
+    <div className={style.login}>
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            required
-            autoComplete="off"
-            placeholder="email"
-            value={formState.email}
-            onChange={handleInutChange}
-          />
-          <input
-            type="password"
-            name="password"
-            required
-            autoComplete="off"
-            placeholder="password"
-            value={formState.password}
-            onChange={handleInutChange}
-          />
-          <button type="submit">Log in</button>
-        </form>
+        <div className={style.login_wrapper}>
+          <form className={style.form} onSubmit={handleSubmit}>
+            <label className="form_label" htmlFor="email">
+              <EmailICon className="form_input_icon" />
+              <input
+                type="email"
+                name="email"
+                required
+                autoComplete="off"
+                placeholder="email"
+                className="form_input"
+                title="Invalid email address"
+                value={formState.email}
+                id="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                onChange={handleInutChange}
+              />
+            </label>
+            <label className="form_label" htmlFor="password">
+              <KeyIcon className="form_input_icon" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                autoComplete="off"
+                placeholder="password"
+                className="form_input"
+                value={formState.password}
+                pattern="^[A-Za-z0-9]{8,16}$"
+                title="Invalid password, should be more then 8 character and less then 16"
+                onChange={handleInutChange}
+              />
+            </label>
+            <button className="form_btn" type="submit">Log In</button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -5,6 +5,9 @@ import { useNavigate } from 'react-router-dom';
 
 import { logIn } from '../../api/logIn';
 import { signIn } from '../../api/signIn';
+import { ReactComponent as EmailICon } from '../../assets/svg/auth/email.svg';
+import { ReactComponent as KeyIcon } from '../../assets/svg/auth/key.svg';
+import { ReactComponent as UserIcon } from '../../assets/svg/auth/user.svg';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { ICreateUser } from '../../types/ICreateUser';
@@ -54,36 +57,59 @@ export const SignIn = () => {
   return (
     <div className={style.auth}>
       <div className="container">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            required
-            autoComplete="off"
-            placeholder="name"
-            value={formState.name}
-            onChange={handleInutChange}
-          />
-          <input
-            type="email"
-            name="email"
-            required
-            autoComplete="off"
-            placeholder="email"
-            value={formState.email}
-            onChange={handleInutChange}
-          />
-          <input
-            type="password"
-            name="password"
-            required
-            autoComplete="off"
-            placeholder="password"
-            value={formState.password}
-            onChange={handleInutChange}
-          />
-          <button type="submit">Register</button>
-        </form>
+        <div className={style.auth_wrapper}>
+          <form className={style.form} onSubmit={handleSubmit}>
+            <label className="form_label" htmlFor="name">
+              <UserIcon className="form_input_icon" />
+              <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                autoComplete="off"
+                placeholder="name"
+                className="form_input"
+                pattern="^[A-Za-z0-9]{3,16}$"
+                value={formState.name}
+                title="Invalid name, should be more then 3 character and less then 16"
+                onChange={handleInutChange}
+              />
+            </label>
+            <label className="form_label" htmlFor="email">
+              <EmailICon className="form_input_icon" />
+              <input
+                type="email"
+                name="email"
+                required
+                autoComplete="off"
+                placeholder="email"
+                className="form_input"
+                title="Invalid email address"
+                value={formState.email}
+                id="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                onChange={handleInutChange}
+              />
+            </label>
+            <label className="form_label" htmlFor="password">
+              <KeyIcon className="form_input_icon" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                autoComplete="off"
+                placeholder="password"
+                className="form_input"
+                value={formState.password}
+                pattern="^[A-Za-z0-9]{8,16}$"
+                title="Invalid password, should be more then 8 character and less then 16"
+                onChange={handleInutChange}
+              />
+            </label>
+            <button className="form_btn" type="submit">Register</button>
+          </form>
+        </div>
       </div>
     </div>
   );
