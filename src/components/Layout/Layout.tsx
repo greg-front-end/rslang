3e27205391edb95712390186bcf7b4c96785e9c1
Greg <martinstudyweb@gmail.com>
2022-09-03 03:16:43 +0300

    feat: add custom hook for log out after token expired

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { useAppSelector } from '../../hooks/useAppSelector';
+import { useLogOutAfterTokenExp } from '../../hooks/useLogOutAfterTokenExp';
 import { isUserLogIn } from '../../utils/isUserLogIn';
 import { FooterLogIn } from '../Footer/FooterLogIn/FooterLogIn';
 import { FooterLogOut } from '../Footer/FooterLogOut/FooterLogOut';
@@ -9,9 +10,8 @@ import { HeaderLogIn } from '../Header/HeaderLogIn/HeaderLogIn';
 import { HeaderLogOut } from '../Header/HeaderLogOut/HeaderLogOut';
 
 export const Layout = () => {
-  const userAuthState = useAppSelector((state) => state.auth);
   const isActiveSideBar = useAppSelector((state) => state.sideBar.isActiveSideBar);
-  const [isUserLoggined, setIsUserLoggined] = useState(false);
+  const isUserLoggined = useLogOutAfterTokenExp();
   let activeClass = 'main_wrapper_log_out';
   if (isActiveSideBar && isUserLogIn()) {
     activeClass = 'main_wrapper_log_in header_active';
@@ -20,14 +20,6 @@ export const Layout = () => {
   } else {
     activeClass = 'main_wrapper_log_out';
   }
-  useEffect(() => {
-    if (userAuthState.token) {
-      setIsUserLoggined(isUserLogIn());
-    }
-    if (!userAuthState.token) {
-      setIsUserLoggined(isUserLogIn());
-    }
-  }, [userAuthState.token]);
   return (
     <div className={activeClass}>
       {isUserLoggined ? <HeaderLogIn /> : <HeaderLogOut />}
